19641dcbc069297c51f34e6a93ec97793ea04ebd
JavaBorka <borka34@gmail.com>
2023-11-12 20:15:40 +0100

    Založený objekt s routami

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,20 @@ import React from 'react'
 import App from './App.jsx'
 import './index.css'
 
+import {
+  createBrowserRouter,
+  RouterProvider
+} from 'react-router-dom'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />
+  }
+])
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
